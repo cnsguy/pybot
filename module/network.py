2ab26eb31529a5e50f4db1894f3c5759acef6e35
cnsguy <82461731+cnsguy@users.noreply.github.com>
2022-06-28 15:23:24 +0200

    Module network: strip !update stuff

--- module/network.py
+++ module/network.py
@@ -4,12 +4,6 @@ import core.bot_instance
 import core.irc_packet
 from subprocess import Popen, PIPE
 
-def get_latest_commit(repo):
-    proc = Popen(["git", "log", repo, "main", "--oneline", "--no-color"], stdout = PIPE, encoding = "u8")
-    out, _ = proc.communicate()
-    out = out.split("\n")
-    return out[0].strip()
-
 class ModuleMain(core.module.Module):
     def __init__(self, bot, name):
         super().__init__(bot, name)
@@ -19,9 +13,6 @@ class ModuleMain(core.module.Module):
         self.register_command(
             core.command.Command("restart", self.handle_restart_command, 0, "(<reason...>)",
                 "Restarts the bot.", "network.restart"))
-        self.register_command(
-            core.command.Command("update", self.handle_update_command, 0, None,
-                "Checks git origin/main for updates and restarts the bot if there are any.", "network.update"))
 
     def handle_quit_command(self, source, target, is_pm, args):
         reason = " ".join(args)
@@ -41,17 +32,4 @@ class ModuleMain(core.module.Module):
         else:
             self.bot.send_immediately("QUIT")
 
-        self.bot.restart()
-
-    def handle_update_command(self, source, target, is_pm, args):
-        proc = Popen(["git", "fetch", "origin"])
-        proc.communicate()
-        last_local = get_latest_commit("main")
-        last_remote = get_latest_commit("origin/main")
-
-        if last_remote == last_local:
-            self.bot.send_message(target, "No new commits on origin/main.")
-            return
-
-        self.bot.send_immediately("QUIT :Updating to %s" % last_remote)
         self.bot.restart()
